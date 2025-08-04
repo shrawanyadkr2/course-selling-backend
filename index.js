1c186ac929b3487b4ba32fcec68dd7bb193a100f
shrawan <shrawan29yadav@gmail.com>
2025-08-04 08:50:40 +0530

    step 3 run the port if the database is connected

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 
-const {userRouter} = require("./routes/user");
-const {courseRouter} = require("./routes/course");
-const {adminRouter}= require("./routes/admin");
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/course");
+const { adminRouter } = require("./routes/admin");
+const { mongo, default: mongoose } = require('mongoose');
 
-app.use("api/v1/user",userRouter);
-app.use("api/v1/course",courseRouter);
-app.use("api/v1/admin",adminRouter);
+app.use("api/v1/user", userRouter);
+app.use("api/v1/course", courseRouter);
+app.use("api/v1/admin", adminRouter);
 
-app.listen(3000);
+async function main() {
+   await mongoose.connect("mongodb+srv://admin:ftc5w1ttoyEOAWDV@cluster0.uyjza.mongodb.net/coursera-app");
+
+    app.listen(3000);
+
+    console.log("app is listening and connected as well");
+
+}
+
+main();
 
 
 
